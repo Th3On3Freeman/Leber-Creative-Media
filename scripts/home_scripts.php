33ed8df3791e57fa9c244b4280170361ff4bbc9b
Th3On3Freeman <michael.leber1@gmail.com>
2016-11-03 23:24:32 -0400

    End of Day 11/3/16
    Added Contact Form

--- scripts/home_scripts.php
+++ scripts/home_scripts.php
@@ -9,5 +9,9 @@
 <!--jQuery Easing (JavaScript Plugin)-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
+<!-- Contact Form JavaScript -->
+<script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/includes/Bootstrap Themes/Start Bootstrap/js/jqBootstrapValidation.js"></script>
+<script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/includes/Bootstrap Themes/Start Bootstrap/js/contact_me.js"></script>
+
 <!-- Theme JavaScript -->
 <script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/includes/Bootstrap Themes/Start Bootstrap/js/freelancer.min.js"></script>
